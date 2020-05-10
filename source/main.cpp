7778f60a0c633432e417837e58fd7e02e2949c3a
NixAJ <nickiaj@live.dk>
2020-05-10 20:48:00 +0200

    Updated Main

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,32 @@
 #include <pch/Build.h>
 #include <iostream>
 
+//#include "Lexer/Lexer.h"
+
 int main()
 {
-#ifdef NAI_CLANG
-    std::cout << "Hello Clang!";
-#else
-    std::cout << "Hello World!";
-#endif
+    FILE* file = nullptr;
+    fopen_s(&file, "text.nai", "r");
+    if (file)
+    {
+        fseek(file, 0, SEEK_END);
+        unsigned long size = ftell(file);
+        rewind(file);
+
+        char* buffer = new char[sizeof(char) * size];
+        if (buffer)
+        {
+            size_t result = fread(buffer, 1, size, file);
+            if (result)
+            {
+               /* Lexer lexer;
+                lexer.Init(buffer, result);
+                lexer.Process();*/
+            }
+        }
+
+        fclose(file);
+    }
+
     return 0;
 }
